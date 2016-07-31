38094e6292006c78f6f82ac0d937811cb082b6ba
alyssalingad <alyssalingad@gmail.com>
2016-07-30 18:11:33 -0700

    added start screen and home screen

---
+++ ATTHackProject/app/src/main/java/com/example/elise/atthackproject/Home.java
@@ -0,0 +1,15 @@
+package com.example.elise.atthackproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Alyssa on 7/30/2016.
+ */
+public class Home extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+    }
+}
--- ATTHackProject/app/src/main/java/com/example/elise/atthackproject/MainActivity.java
+++ ATTHackProject/app/src/main/java/com/example/elise/atthackproject/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.elise.atthackproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
@@ -11,6 +13,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button) findViewById(R.id.signup);
+        Button button = (Button) findViewById(R.id.signUp);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), Home.class);
+                v.getContext().startActivity(intent);
+            }
+        });
     }
 }
